9dd3356ce6d9fe163cc81e18c7efa212e5bda5ba
Philip Matesanz <philip.matesanz@driggle.com>
2018-05-11 16:03:09 +0200

    PopulatePackets method

--- create.py
+++ create.py
@@ -1,5 +1,28 @@
 from voltdb import FastSerializer, VoltProcedure
-import json
+import json, uuid, datetime
+
+def populate_packets(client, data):
+    proc = VoltProcedure(client, "PACKET.insert", [
+            FastSerializer.VOLTTYPE_STRING, FastSerializer.VOLTTYPE_STRING,
+            FastSerializer.VOLTTYPE_STRING, FastSerializer.VOLTTYPE_INTEGER,
+            FastSerializer.VOLTTYPE_INTEGER, FastSerializer.VOLTTYPE_STRING,
+            FastSerializer.VOLTTYPE_TIMESTAMP, FastSerializer.VOLTTYPE_STRING,
+            FastSerializer.VOLTTYPE_STRING
+        ])
+
+    for d in data:
+        flag = ""
+        if d["SYN"]:
+            flag = "s"
+        if d["FIN"]:
+            flag = "f"
+        
+        dt = datetime.datetime.fromtimestamp(d["ts"])
+        res = proc.call([
+            str(uuid.uuid4()), str(d.get("srcIP", "")), str(d.get("dstIP", "")),
+            int(d.get("srcPort", "")), int(d.get("dstPort", "")), str(d.get("payload", "")),
+            dt, flag, str(d.get("type", "")) 
+        ])
 
 def populate_dbn(client, data):
     pass
@@ -10,5 +33,6 @@ with open ("resources/tcpdump.json", "r") as f:
     data=f.read()
     j_data = json.loads(data)
     populate_dbn(client, j_data)
+    populate_packets(client, j_data)
 
     client.close()
