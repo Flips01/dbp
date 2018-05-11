2f191a35c226a5e876a3009a1721e6b59188d42e
Tobias <tobias.baden@t-online.de>
2018-05-11 16:11:59 +0200

    popilate_ip_connections

--- create.py
+++ create.py
@@ -38,15 +38,19 @@ def populate_packets(client, data):
             flag = "s"
         if d["FIN"]:
             flag = "f"
-        
+
         dt = datetime.datetime.fromtimestamp(d["ts"])
         res = proc.call([
             str(uuid.uuid4()), str(d.get("srcIP", "")), str(d.get("dstIP", "")),
             int(d.get("srcPort", "")), int(d.get("dstPort", "")), str(d.get("payload", "")),
-            dt, flag, str(d.get("type", "")) 
+            dt, flag, str(d.get("type", ""))
         ])
 
-
+def populate_ip_connections(client, data):
+    TABLENAME = "IP_CONNECTIONS"
+    proc = VoltProcedure(client, TABLENAME+".insert", [FastSerializer.VOLTTYPE_STRING, FastSerializer.VOLTTYPE_INTEGER, FastSerializer.VOLTTYPE_STRING])
+    for d in data:
+        response = proc.call([str(d.get("dstIP", "")), int(d.get("dstPort", "")), str(d.get("srcIP", ""))])
 
 with open ("resources/tcpdump.json", "r") as f:
     client = FastSerializer("192.168.58.3", 21212)
@@ -56,4 +60,5 @@ with open ("resources/tcpdump.json", "r") as f:
     populate_dbn(client, j_data)
     populate_packets(client, j_data)
     populate_SYNFIN_table(client,j_data)
+    populate_ip_connections(client,j_data)
     client.close()
