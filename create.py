52d1c24e6e3d15ccb1b6133b4e116fb59e399ae6
Philip Matesanz <philip.matesanz@driggle.com>
2018-05-11 15:22:58 +0200

    VoltDB Client eingefügt

--- create.py
+++ create.py
@@ -1,9 +1,14 @@
+from voltdb import FastSerializer, VoltProcedure
 import json
 
-def populate_dbn(data):
+def populate_dbn(client, data):
     pass
 
 with open ("resources/tcpdump.json", "r") as f:
+    client = FastSerializer("192.168.58.3", 21212)
+
     data=f.read()
     j_data = json.loads(data)
     populate_dbn(j_data)
+
+    client.close()
