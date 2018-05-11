2d9a8ef92e872511200595535e6195c0ba3a2327
yannic <yannickuester@gmail.com>
2018-05-11 15:45:30 +0200

    added populate_SYNFIN_table function

--- create.py
+++ create.py
@@ -10,6 +10,19 @@ def populate_ADV_table(client, data):
     for d in data:
         response = proc.call([d["srcIP"], d["dstIP"], 1, d["size"]])
 
+def populate_SYNFIN_table(client, data):
+    TABLENAME = "SYN_FIN_RATIO"
+    proc = VoltProcedure(client, TABLENAME+".insert", [FastSerializer.VOLTTYPE_TIMESTAMP, FastSerializer.VOLTTYPE_TIMESTAMP, FastSerializer.VOLTTYPE_STRING])
+    for d in data:
+        if d["FIN"] or d["SYN"]:
+            flag = ""
+            if d["FIN"]:
+                flag = "F"
+            elif d["SYN"]:
+                flag = "S"
+            time = d["ts"]*1000+d["ms"]
+            response = proc.call([time//(1000*60*60), time, flag])
+
 
 with open ("resources/tcpdump.json", "r") as f:
     client = FastSerializer("192.168.58.3", 21212)
