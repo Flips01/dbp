49094fec3cae19dddf2002da9d65d1a1dcd8dd0d
Philip Matesanz <philip.matesanz@driggle.com>
2018-05-11 15:01:42 +0200

    Schleifen-Bug gefixxt

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from voltdb import FastSerializer, VoltProcedure
 
 client = FastSerializer("192.168.58.3", 21212)
 proc = VoltProcedure(client, "TEST2.insert", [FastSerializer.VOLTTYPE_INTEGER, FastSerializer.VOLTTYPE_STRING])
-response = proc.call([i, "English"])
+response = proc.call([1, "English"])
 client.close()
 
 print response
