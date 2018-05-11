66b1289821c3142c441f8a23030177ff0e8e845c
yannic <yannickuester@gmail.com>
2018-05-11 15:33:42 +0200

    added population for average data volume table (requires server-side stored procedure!)

--- create.py
+++ create.py
@@ -4,6 +4,13 @@ import json
 def populate_dbn(client, data):
     pass
 
+def populate_ADV_table(client, data):
+    TABLENAME = "Average_Data_Volume"
+    proc = VoltProcedure(client, TABLENAME+".insert", [FastSerializer.VOLTTYPE_STRING, FastSerializer.VOLTTYPE_STRING, FastSerializer.VOLTTYPE_INTEGER, FastSerializer.VOLTTYPE_INTEGER])
+    for d in data:
+        response = proc.call([d["srcIP"], d["dstIP"], 1, d["size"]])
+
+
 with open ("resources/tcpdump.json", "r") as f:
     client = FastSerializer("192.168.58.3", 21212)
 
