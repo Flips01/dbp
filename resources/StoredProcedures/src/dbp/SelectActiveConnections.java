f4d6aca5e58648214620d72e460d04f01a13bc31
yannic <yannickuester@gmail.com>
2018-05-24 18:01:06 +0200

    added selectActiveConnections query sp added selectAverageDataVolume query sp added selectByteSequence query sp minor text fixes

--- resources/StoredProcedures/src/dbp/SelectActiveConnections.java
+++ resources/StoredProcedures/src/dbp/SelectActiveConnections.java
@@ -6,21 +6,23 @@ import org.voltdb.VoltTable;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 public class SelectActiveConnections extends VoltProcedure{
 
+    public final SQLStmt singleSelect = new SQLStmt("SELECT SRC_IP, SRC_PORT, DST_IP, DST_PORT FROM Connections WHERE Timestamp_Range = ? AND Timestamp between ? AND ?");
     public VoltTable[] run(Timestamp timestamp){
-        final SQLStmt singleSelect = new SQLStmt("SELECT SRC_IP, SRC_PORT, DST_IP, DST_PORT FROM Connections "
-                +"WHERE Timestamp_Range = ? AND Timestamp between ? AND ?");
         LocalDateTime before = timestamp.toLocalDateTime().minusMinutes(1);
         LocalDateTime after = timestamp.toLocalDateTime().plusMinutes(1);
+        Timestamp beforeTS = Timestamp.from(before.toInstant(ZoneOffset.ofHours(0)));
+        Timestamp afterTS = Timestamp.from(after.toInstant(ZoneOffset.ofHours(0)));
         if(before.getHour() != after.getHour()){
-            voltQueueSQL(singleSelect, timestamp.getTime()/(1000*60*60),before, after);
+            voltQueueSQL(singleSelect, timestamp.getTime()/(1000*60*60),beforeTS, afterTS);
         }else{
             long beforeTime = timestamp.getTime()-60*1000;
             long afterTime = timestamp.getTime()+60*1000;
-            voltQueueSQL(singleSelect, beforeTime/(1000*60*60),before, timestamp);
-            voltQueueSQL(singleSelect, afterTime/(1000*60*60), timestamp, afterTime);
+            voltQueueSQL(singleSelect, beforeTime/(1000*60*60),beforeTS, timestamp);
+            voltQueueSQL(singleSelect, afterTime/(1000*60*60), timestamp, afterTS);
         }
         return voltExecuteSQL();
     }
---
+++ resources/StoredProcedures/src/dbp/SelectByteSequence.java
@@ -0,0 +1,14 @@
+package dbp;
+
+import org.voltdb.SQLStmt;
+import org.voltdb.VoltProcedure;
+import org.voltdb.VoltTable;
+
+public class SelectByteSequence extends VoltProcedure{
+    public final SQLStmt select = new SQLStmt("SELECT * FROM PAYLOAD P WHERE P.PAYLOAD LIKE ?");
+
+    public VoltTable[] run(String payload){
+        voltQueueSQL(select, "%"+payload+"%");
+        return voltExecuteSQL();
+    }
+}
