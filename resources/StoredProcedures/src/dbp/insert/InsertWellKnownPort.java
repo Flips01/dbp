da97ff17f6e56e791194cc8fec6c3c3ef12bc2d4
yannic <yannickuester@gmail.com>
2018-05-31 10:41:54 +0200

    fixed unrequired parameters for run method of InsertWellKnownPort

--- resources/StoredProcedures/src/dbp/insert/InsertWellKnownPort.java
+++ resources/StoredProcedures/src/dbp/insert/InsertWellKnownPort.java
@@ -11,7 +11,7 @@ public class InsertWellKnownPort extends VoltProcedure {
     public final SQLStmt insertWellKnownPort = new SQLStmt("INSERT INTO WELL_KNOWN_PORTS VALUES(?, ?)");
 
 
-    public long run(Timestamp time, String srcIP, String dstIP, int srcPort, int dstPort, String flag, String payload, String type, int size) {
+    public long run(String dstIP, int dstPort) {
         final long START_TIME = System.currentTimeMillis();
 
         WKPQuery wkp = WKPQuery.getInstance();
