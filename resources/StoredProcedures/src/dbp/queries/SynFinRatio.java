b9eda902dad57738757129281a5cb42c35125f42
florian <florian.rueckauf@stud.hs-hannover.de>
2018-06-07 14:39:22 +0200

    update calculation of TimeRange

--- resources/StoredProcedures/src/dbp/queries/SynFinRatio.java
+++ resources/StoredProcedures/src/dbp/queries/SynFinRatio.java
@@ -3,11 +3,10 @@ package dbp.queries;
 import org.voltdb.*;
 
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+
 
 /*
-RRetrieve the ratio of SYN packets to FIN packets in a given time period
+Retrieve the ratio of SYN packets to FIN packets in a given time period
 (only TCP packets)
 
  */
@@ -24,16 +23,13 @@ public class SynFinRatio extends VoltProcedure{
 
     public VoltTable run(Timestamp startTime, Timestamp endTime) throws VoltAbortException{
 
-		long startHour = partitionTs((int)startTime.getTime()/1000, partitionInterval);
-        long endHour = partitionTs((int)endTime.getTime()/1000, partitionInterval);
-		
-		long queryHours = endHour-startHour;
-		
+		int[] partitions = partitionsInRange((int)startTime.getTime()/1000, (int)endTime.getTime()/1000, partitionInterval);
+
 		long synCount = 0;
 		long finCount = 0;
 
-		for (int i = 0; i < queryHours; ++i){
-			voltQueueSQL(getCount, startHour+i, startTime, endTime);
+		for (int i = 0; i < partitions.length; ++i){
+			voltQueueSQL(getCount, partitions[i], startTime, endTime);
         	VoltTable[] result = voltExecuteSQL();
 
 			for(int j = 0; j < result[0].getRowCount(); j++) {
@@ -46,25 +42,34 @@ public class SynFinRatio extends VoltProcedure{
 			}
         }
 
-        VoltTable result = new VoltTable(new VoltTable.ColumnInfo("SynFinRatio", VoltType.FLOAT), new VoltTable.ColumnInfo("startHour",VoltType.INTEGER), new VoltTable.ColumnInfo("endHour",VoltType.INTEGER));
+        VoltTable result = new VoltTable(new VoltTable.ColumnInfo("SynFinRatio", VoltType.FLOAT));
 
         if(finCount > 0){
-			result.addRow(synCount/(float)finCount, startHour, endHour);
+			result.addRow(synCount/(float)finCount);
 		}else{
-			result.addRow((float)synCount, startHour, endHour);
+			result.addRow((float)synCount);
 		}
 
 		return result;
     }
 
-	private long convertTimestamp(Timestamp ts){
-		LocalDateTime timeLocal = ts.toLocalDateTime();
-        timeLocal = timeLocal.minusMinutes(timeLocal.getMinute()).minusSeconds(timeLocal.getSecond()).minusNanos(timeLocal.getNano());
-        //Dangerous time magic
-        Timestamp hour = Timestamp.from(timeLocal.toInstant(ZoneOffset.ofHours(0)));
+    //Create array of Timestamp_Ranges the query data are in
 
-		return hour.getTime()/(1000*60*60);
-	}
+    private int[] partitionsInRange(int start, int end, int partitionInterval) {
+        int start_part = partitionTs(start, partitionInterval);
+        int end_part = partitionTs(end, partitionInterval);
+
+        int part_range = end_part-start_part;
+        int partitions = (part_range/partitionInterval)+1;
+
+        int[] parts = new int[partitions];
+        for(int i=0; i<partitions; i++) {
+            parts[i] = start_part + (i*partitionInterval);
+        }
+        return parts;
+    }
+
+    //Converts Timestamp to Timestamp_Range
 
 	private int partitionTs(int ts, int partitionInterval) {
 		int p = ts / partitionInterval;
